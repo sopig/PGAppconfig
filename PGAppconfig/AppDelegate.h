ab36023228fde038323700e9ba0f6678588c7bdd
张正超 <chay0103@163.com>
2016-04-20 14:47:12 +0800

    init

---
+++ PGAppconfig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PGAppconfig
+//
+//  Created by 张正超 on 16/4/20.
+//  Copyright © 2016年 jiuxian.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
